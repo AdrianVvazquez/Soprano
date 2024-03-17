b73fe925bc9dd410eb77ccc81292b4f4d66dafed
AdrianVvazquez <jose_a_vazquez@outlook.com>
2024-03-17 15:57:56 -0600

    Argumentos de entrada y excepciones

--- soprano.py
+++ soprano.py
@@ -2,7 +2,7 @@ import sys
 from antlr4 import *
 from sopranoLexer import sopranoLexer
 from sopranoParser import sopranoParser
-from visitor import Visitor
+from visitor import Visitor, bcolors, SopranoException
 import sys
 
 input_stream = FileStream(sys.argv[1], encoding='utf-8')
@@ -12,9 +12,13 @@ token_stream = CommonTokenStream(lexer)
 parser = sopranoParser(token_stream)
 tree = parser.root()
 
+if sys.argv[1].rsplit(".", 1)[1] != "sopr":
+    print(bcolors.FAIL+"[Error] Bad extension."+bcolors.RESET)
+    sys.exit(0)
+
 if len(sys.argv) > 2:
-    visitor = Visitor(sys.argv[2])
+    visitor = Visitor(file_name=sys.argv[1], entryProc=sys.argv[2])
 else:
-    visitor = Visitor()
+    visitor = Visitor(file_name=sys.argv[1])
 
 visitor.visit(tree)
